712d23bdcb8c610d9382b3e732e9895fe8c1e54b
xiaomo <83387856@qq.com>
2016-08-02 17:07:26 +0800

    移到首页

--- src/app/web_app/xiaomo-app.component.ts
+++ src/app/web_app/xiaomo-app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { WebNavbarComponent } from './web_navbar';
+import { WebNavbarComponent } from './web_home/web_navbar';
 import { WebFooterComponent, WebHeaderComponent } from 'common';
 
 @Component({

6fb9eb89a045b6eefb877e0dc55ab68e33a740c5
xiaomo <83387856@qq.com>
2016-08-02 17:05:22 +0800

    一句话介绍

--- src/app/web_app/web_home/web-home.component.ts
+++ src/app/web_app/web_home/web-home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
 import { WebBannerComponent } from './web_banner';
+import { WebIntroduceComponent } from './web_introduce';
+
 
 @Component({
   selector: 'web-home',
   providers: [],
-  directives: [WebBannerComponent],
+  directives: [WebBannerComponent,WebIntroduceComponent],
   pipes: [],
   styles: [require('./web-home.scss')],
   template: require('./web-home.html')

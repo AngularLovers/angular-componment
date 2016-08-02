64066a88229ed3b92f0d3122b29c9e5f42197875
xiaomo <83387856@qq.com>
2016-08-02 10:11:12 +0800

    去掉多余依赖

--- src/app/web-app/web-about/web-about.component.ts
+++ src/app/web-app/web-about/web-about.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AboutService } from './web-about.service';
-import { ArticleItemComponent } from '../article-item';
 
 @Component({
   selector: 'web-about',
   providers: [AboutService],
-  directives: [ArticleItemComponent],
   styles: [],
   template: require('./web-about.html')
 })

cf92702c375a61277a7ef5eaa1e3e441fbb5e051
xiaomo <83387856@qq.com>
2016-08-11 15:18:15 +0800

    api文档

--- src/app/common/commonRadios/index.ts
+++ src/app/common/commonRadios/index.ts
@@ -1,21 +1,18 @@
 export * from './common-radio.component';
 
 /**
- *    按钮
+ *  radios:Array<Object>
+ *    里面要多少个选项就传多少个name
+ *  [
+ *   {
+ *    "name": "北京"
+ *   },
+ * ]
  *
- *   msg 内容
- *   color 颜色   btn-success|btn-default|btn-danger|btn-info|btn-primary
- *   size 大小    btn-sm|btn-lg|btn-xxx
- *   isClickAble 是否可点击  默认可点击  true|false 默认false
- *   icon 图标  不传就没有图标
- *   isRadius 是否是圆角
+ * 使用
+ * <common-radio [radios]="radios" ></common-radio>
  *
- *  父组件中定义些方法然后供子组件调用
- *  clickEvent(){
- *   // todo
- *  }
  *
- *   使用
- *   <common-button (clickEvent)="clickEvent()" [msg]="'测试'" [color]="'btn-danger'" [isRadius]="true"></common-button>
  *
+ * 鸣谢 @Alice的样式和思路
  * */

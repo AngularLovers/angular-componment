4e86fbac7907cac3e6cd4f24f28f1b3cd07353b3
xiaomo <83387856@qq.com>
2016-08-12 11:03:36 +0800

    alert新api

--- src/app/common/commonAlerts/index.ts
+++ src/app/common/commonAlerts/index.ts
@@ -1,29 +1,12 @@
 export * from './common-alert.component';
 /**
 1.参数：
-alerts: Array<AlertModel> = [];
-
-constructor() {
-  this.alerts = [
-    {
-      type: 'danger',
-      msg: '这是一个提示危险的消息.',
-      dismissTime: 30000,
-      closable: true
-    },
-    {
-      type: 'success',
-      msg: '这是一个提示成功的消息.',
-      dismissTime: 30000,
-      closable: true
-    }
-  ]
-}
+  dismissTime 自动消失的时间/毫秒
+  type 提示类型  danger|info|warning|success
+  msg  提示内容  随便什么文字都可以
 
  2、 使用
 定好以上参数，传给该组件就可以了。
-<common-alert [alerts]="alerts"></common-alert>
-
-
+ <common-alert [dismissTime]="10000" [type]="'danger'"   [msg]="'这是一个提示危险的消息'"></common-alert>
   3.后续可以考虑加上动画
  */

6aef64ffc6a7d3f2089043620c74b62498ccc108
xiaomo <83387856@qq.com>
2016-10-08 16:01:43 +0800

    bootstrap

--- src/app/common/footer/footer.ts
+++ src/app/common/footer/footer.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'xiaomo-footer',
+  styles:[require('./footer.css')],
   template: require('./footer.html')
 })
 export class FooterComponent implements OnInit {

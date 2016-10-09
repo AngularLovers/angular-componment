aa27da1dc4fbf9b29a143cb03948e8c89f80c49f
xiaomo <83387856@qq.com>
2016-10-09 09:07:56 +0800

    head css

--- src/app/common/header/header.ts
+++ src/app/common/header/header.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'xiaomo-header',
+  styles:[require('./header.css')],
   template:require('./header.html')
 })
 export class HeaderComponent implements OnInit {

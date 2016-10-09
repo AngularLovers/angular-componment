f32e624e3237c1ff0fd902dbac08d4a40002ac98
xiaomo <83387856@qq.com>
2016-10-09 11:19:18 +0800

    about me

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,21 +10,8 @@ console.log('`About` component loaded asynchronously');
 
 @Component({
   selector: 'about',
-  styles: [`
-  `],
-  template: `
-    <h1>About</h1>
-    <div>
-      For hot module reloading run
-      <pre>npm run start:hmr</pre>
-    </div>
-    <div>
-      <h3>
-        patrick@AngularClass.com
-      </h3>
-    </div>
-    <pre>this.localState = {{ localState | json }}</pre>
-  `
+  styles: [require('./about.css')],
+  template:require('./about.html')
 })
 export class About {
   localState: any;
